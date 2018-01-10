1ff6ab4bfee28e9de646c9a9111236b34149cbec
javierramirezzayas <javier.ramirez1@upr.edu>
2018-01-09 22:10:15 -0400

    Delete dbconfig.py

--- config/dbconfig.py
+++
@@ -1,9 +0,0 @@
-# Database configuration information
-
-# This is not part of Phase 1 of the Project, but is left here for use in future phases.
-#
-# pg_config = {
-#     'user' : 'student',
-#     'psswd' : 'icom',
-#     'dbname' : 'projectdb'
-# }
